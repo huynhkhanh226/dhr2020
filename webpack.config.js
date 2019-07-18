402e02ecc30e511948ae5e93c2019fd8d75143eb
HUYNH KHANH <HUYNHKHANH@diginetcorp.com>
2019-07-18 08:34:12 +0700

    update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,18 @@
+
+const parts = require("./webpack.parts");
 var path = require('path');// to get the current path
 const fs = require('fs'); // to check if the file exists
 const dotenv = require('dotenv');
-//var merge = require('webpack-merge');
+var merge = require('webpack-merge');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ManifestPlugin = require('webpack-manifest-plugin');
 var TARGET = process.env.npm_lifecycle_event;
 
+  //****************************************************************************************************/
 
-module.exports = (env) => {
-
-  // Get the root path (assuming your webpack config is in the root of your project!)
-  const currentPath = path.join(__dirname);
-  
-  // Create the fallback path (the production .env)
-  const basePath = currentPath + '/.env';
-
-  // We're concatenating the environment name to our filename to specify the correct env file!
-  const envPath = basePath + '.' + env.ENVIRONMENT;
-
-  // Check if the file exists, otherwise fall back to the production .env
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
-  // Set the path parameter in the dotenv config
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-  
-  // reduce it to a nice object, the same as before (but with the variables from the file)
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    console.log(prev);
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
-
-
-  //console.log(env);
-  console.log(TARGET);
-  console.log(fileEnv);
-  console.log(process.env);
-
-  return {
-    entry: path.join(__dirname, './src/index.js'),
+var commonConfig = {
+  entry: path.join(__dirname, './src/index.js'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'bundles.js'
@@ -50,6 +23,10 @@ module.exports = (env) => {
         '@': path.resolve(__dirname, 'src'),
       }
     },
+};
+
+const debugModeModules = merge([
+  {
     module: {
       rules: [
         // this will apply to both plain `.scss` files
@@ -125,28 +102,136 @@ module.exports = (env) => {
         // }
         
       ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        TITLE: process.env.TITLE,
-        PUBLIC_URL: process.env.PUBLIC_URL,
-        template: path.join(__dirname, 'public', 'index.html'),
-        BASE_URL: process.env.PUBLIC_URL
-      }),
-      new webpack.LoaderOptionsPlugin({
-        // test: /\.xxx$/, // may apply this only for some modules
-        // options: {
+    }
+  }
+]);
+
+const productModeModules = merge([
+  {
+    module: {
+      rules: [
+        // this will apply to both plain `.scss` files
+        // AND `<style lang="scss">` blocks in `.vue` files
+         {
+            test: /\.scss$/,
+            loader: ['css-loader','sass-loader']
+        }, 
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|jpg|gif|svg|ico)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/images/',
+          }
+        },
+        {
+          test: /\.(json)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]'
+          }
+        },
+        {
+          test: /\.(ttf|woff|woff2|eot)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/fonts',
+          }
+        },
+  
+        // {
         //   test: /\.html$/,
-        //   publicPath: "/",
-        //   baseUrl: "/"
-        // }
-        options:{
-          //publicPath: "/"
+        //   use: [ {
+        //     loader: 'html-loader',
+        //     options: {
+        //       minimize: true,
+        //       removeComments: false,
+        //       collapseWhitespace: false
+        //     }
+        //   }],
+        // },
+        //  {
+        //   test: /\.(png|jpg|gif)$/i,
+        //   use: [
+        //     {
+        //       loader: 'url-loader',
+        //       options: {
+        //         limit: 8192,
+        //         fallback: 'responsive-loader',
+        //       },
+        //     },
+        //   ],
+        // },
+        // {
+        //   test: /\.css$/i,
+        //   use: ['style-loader', 'css-loader'],
+        // },
+  
+        {
+          test: /\.css$/,
+          loader: ['style-loader', 'css-loader']
         }
-      }),
-      new webpack.DefinePlugin(envKeys),
-      new ManifestPlugin()
-      
-    ]
-  };
-}
+        // {
+        //   test: /\.svg$/,
+        //   loader: ['svg-inline-loader']
+        // }
+        
+      ]
+    }
+  }
+]);
+
+module.exports = env => {
+  
+  var dotEnv =  parts.getEnvKeys(env.ENVIRONMENT);
+  
+  const debugModePluginConfig = merge([
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          TITLE: process.env.TITLE,
+          PUBLIC_URL: process.env.PUBLIC_URL,
+          template: path.join(__dirname, 'public', 'index.html'),
+          BASE_URL: process.env.PUBLIC_URL
+        }),
+        new webpack.LoaderOptionsPlugin(),
+        new webpack.DefinePlugin(dotEnv),
+        new ManifestPlugin()
+      ]
+    }
+  ]);
+
+  const productionModePluginConfig = merge([
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          TITLE: process.env.TITLE,
+          PUBLIC_URL: process.env.PUBLIC_URL,
+          template: path.join(__dirname, 'public', 'index.html'),
+          BASE_URL: process.env.PUBLIC_URL
+        }),
+        new webpack.LoaderOptionsPlugin(),
+        new webpack.DefinePlugin(dotEnv),
+        new ManifestPlugin()
+      ]
+    }
+  ]);
+
+  const devServerConfig = merge([
+    parts.devServer({host: process.env.HOST,port: process.env.PORT})
+ ]);
+  
+  var mode = env.ENVIRONMENT;
+  if (mode === "production") {
+    return merge(commonConfig, productModeModules, productionModePluginConfig, { mode });
+  }
+  return merge(commonConfig, debugModeModules, devServerConfig, debugModePluginConfig, { mode });
+};
+
+
