4894e812b8d3d045069ed8093b728badbb9457aa
HUYNH KHANH <HUYNHKHANH@diginetcorp.com>
2019-07-26 08:43:15 +0700

    update project structure

--- src/helpers/auth-header.js
+++ src/helpers/auth-header.js
@@ -1,3 +1,8 @@
+/*!
+ * @author user <email>
+ * date 07/26/2019
+ * attach auth for the header 
+ */
 export function authHeader() {
     // return authorization header with jwt token
     let user = JSON.parse(localStorage.getItem('user'));
