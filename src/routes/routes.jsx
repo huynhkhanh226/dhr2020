f0f8150658f704382088e751822d5b70cb79fb9a
HUYNH KHANH <HUYNHKHANH@diginetcorp.com>
2019-07-31 13:51:18 +0700

    update project structure

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,19 +1,39 @@
 
+import About from '../components/Examples/router/about';
+import {HomePage} from '../components/HomePage/HomePage.jsx';
 export const routes = [
     {
-      path: "/",
-      exact: true,
-      sidebar: () => <div>home!</div>,
-      main: () => <h2>Home</h2>
-    },
-    {
-      path: "/bubblegum",
-      sidebar: () => <div>bubblegum!</div>,
-      main: () => <h2>Bubblegum</h2>
-    },
-    {
-      path: "/shoelaces",
-      sidebar: () => <div>shoelaces!</div>,
-      main: () => <h2>Shoelaces</h2>
+        path: "/",
+        exact: true,
+        component: () => HomePage,
+        childrend: [
+            {
+                path: "/about",
+                exact: true,
+                component: () => About
+              }
+        ]
     }
   ];
+
+//   // then our route config
+// const routes = [
+//     {
+//       path: "/sandwiches",
+//       component: Sandwiches
+//     },
+//     {
+//       path: "/tacos",
+//       component: Tacos,
+//       routes: [
+//         {
+//           path: "/tacos/bus",
+//           component: Bus
+//         },
+//         {
+//           path: "/tacos/cart",
+//           component: Cart
+//         }
+//       ]
+//     }
+//   ];
